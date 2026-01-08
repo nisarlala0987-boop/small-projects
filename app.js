a0e471777eec5de47227d33ea085936798485c96
nisarlala0987-boop <nisarlala0987@gmail.com>
2026-01-08 13:29:04 +0500

    added code

--- app.js
+++ app.js
@@ -18,13 +18,42 @@ function incre() {
     counterElement.innerText = count;        
 }
 
-function password(params) {
-    var password = document.querySelector('#input');
+
+function Password(params) {
     
+    var passwords = document.getElementById('input');
+    var icon = document.getElementById('eye');
+    if (passwords.type==="password") {
+        passwords.type = "text";
+    } else {
+        passwords.type = "password";
+    }
 }
 
+function checking() {
+  var n= document.getElementById("taskInput").value;
+    if(n==="") {
+        
+        document.getElementById("taskInput").value = "";
+        document.getElementById("result").innerText = "";
+        alert("Please enter a number");
+        return;
+       
+
+    }
+   else if (n%2==0) {
+ document.getElementById("result").innerText="Even hai";
+
+    }
+ 
+
+else {
+        document.getElementById("result").innerText= "Odd hai";
+
+    }
 
 
 
+}
 
 
